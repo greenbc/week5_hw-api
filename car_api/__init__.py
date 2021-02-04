35f294daf4387517a0c5169fc59dff3bed2cf387
Ben Green <greenbc005@gmail.com>
2021-02-03 21:33:42 -0500

    added google authentication

--- car_api/__init__.py
+++ car_api/__init__.py
@@ -4,10 +4,18 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from flask_login import LoginManager
+
+from authlib.integrations.flask_client import OAuth
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'signin'
+
+oauth = OAuth(app)
 
 from car_api import routes, models
--- car_api/models.py
+++ car_api/models.py
@@ -1,10 +1,18 @@
-from car_api import app, db
+from car_api import app, db, login_manager
 import uuid
 from datetime import datetime
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+import secrets
+
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
     id = db.Column(db.String, primary_key = True)
     first_name = db.Column(db.String(150), nullable = True, default='')
     last_name = db.Column(db.String(150), nullable = True, default='')
@@ -13,13 +21,19 @@ class User(db.Model):
     g_auth_verify = db.Column(db.Boolean, default = False)
     token = db.Column(db.String, default='')
     date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    drone = db.relationship('Car', backref = 'owner', lazy = True)
 
-    def __init__(self, email, first_name='', last_name='',id = '',password=''):
+    def __init__(self, email, first_name='', last_name='',id = '',password='', token = '', g_auth_verify = False):
         self.id = self.set_id()
         self.first_name = first_name
         self.last_name = last_name
         self.password = self.set_password(password)
         self.email = email
+        self.token = self.set_token(24)
+        self.g_auth_verify = g_auth_verify
+
+    def set_token(self, length):
+        return secrets.token_hex(length)
 
     def set_id(self):
         return str(uuid.uuid4())
@@ -30,3 +44,30 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.email} has been added to the database'
+
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    make = db.Column(db.String(150))
+    model = db.Column(db.String(150))
+    color = db.Column(db.String(150))
+    price = db.Column(db.Integer)
+    user_id = db.Column(db.String, db.ForeignKey('user.id'), nullable = False)
+
+    def __init__(self,make,model,color,price,user_id):
+        self.make = make
+        self.model = model
+        self.color = color
+        self.price = price
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f'The following Car has been added: {self.make} {self.model} which belongs to {self.user_id}'
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "make": self.make,
+            "model": self.model,
+            "color": self.color,
+            "price": self.price
+        }
